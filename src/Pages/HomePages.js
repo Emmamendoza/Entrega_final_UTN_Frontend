009702d7985d4322511013b1b058ad94bb954d2c
Emmanuel Mendoza <Emmamendoza@gmail.com>
2020-12-28 00:18:32 -0300

    Test

---
+++ src/Pages/HomePages.js
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from "react"
+
+
+function HomePages(){
+    const prueba = "Esto es un codigo de prueba de React"
+    //const img_default = <img src={user.avatarUrl}></img>
+      
+    return(
+        <>
+        <div>{prueba}</div>
+        </>
+    )
+}
+export default HomePages
